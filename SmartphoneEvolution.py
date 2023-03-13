7e9cf8dad3bb70966e5f2adde190af7c83a4f1b9
Bryan Valerio Reyes <fsocietybvr@gmail.com>
2023-03-13 17:07:10 -0600

    version 1.1

--- SmartphoneEvolution.py
+++ SmartphoneEvolution.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import streamlit as st
 import re
-import matplotlib.pyplot as plt
 import plotly.express as px
 
 st.set_page_config(page_title="Smartphones", page_icon=":phone:")
